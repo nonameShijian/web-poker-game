5a64baa2891c27c797390a8108737ac1d413395b
inith271 <oy.zf@qq.com>
2023-04-24 22:15:19 +0800

    service: 后端第1次提交，暂时使用当前的文件结构，缺少数据库配置

---
+++ service/src/main/java/top/pokerGame/pokerGame/PokerGameApplication.java
@@ -0,0 +1,13 @@
+package top.pokerGame.pokerGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokerGameApplication.class, args);
+    }
+
+}
---
+++ service/src/test/java/top/pokerGame/pokerGame/PokerGameApplicationTests.java
@@ -0,0 +1,13 @@
+package top.pokerGame.pokerGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
