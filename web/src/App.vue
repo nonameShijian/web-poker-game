7e973e86c27bdb7caddf4bff7ba729471dbb77ff
shijian <2954700422@qq.com>
2023-04-24 15:04:06 +0800

    添加web端模板

---
+++ web/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <div>Vite + Vue</div>
+</template>
+
+<script setup lang="ts">
+import Poker from './util/poker';
+console.log(Poker);
+</script>
+
+<style scoped>
+</style>
---
+++ web/src/views/index.vue
@@ -0,0 +1,8 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+import Poker from '../util/poker';
+console.log(Poker);
+</script>
